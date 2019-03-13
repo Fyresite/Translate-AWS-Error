249c87c7ce681e6fa3833f08606b1cbdfce8dd4b
Stephen Orr <stephen@fyresite.com>
2019-03-13 15:25:22 -0700

    Added test for something

--- index.test.js
+++ index.test.js
@@ -1,5 +1,17 @@
 const translateAwsError = require('./index');
 
+test('Test a code that doesn\'t exist', () => {
+    const error = {
+        code: 'ThisCodeWillNeverExistPorkrinds',
+        message: 'Hey, great job. This is the correct message'
+    };
+
+    expect(translateAwsError(error)).toEqual({
+        code: 'ThisCodeWillNeverExistPorkrinds',
+        message: 'Hey, great job. This is the correct message'
+    });
+});
+
 test('tests a User not found exception', () => {
     const error = {
         code: 'UserNotFoundException'
